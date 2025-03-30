aa413209e1cc580464df32fc4133891e071ea952
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2025-03-30 15:08:30 +0530

    changes

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ import authConfig from '@/auth.config'
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	adapter: PrismaAdapter(prisma) as any,
 	...authConfig,
+	trustHost: true,
 	session: {
 		strategy: 'jwt',
 	},
