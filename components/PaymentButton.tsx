a0a0ff21a63bbbc9f5908b8aa6dfb82f6ae4aff5
Vansh Jhamb <99422155+vanshjhamb9@users.noreply.github.com>
2025-08-07 14:57:58 +0530

    Update PaymentButton.tsx

--- components/PaymentButton.tsx
+++ components/PaymentButton.tsx
@@ -2,16 +2,19 @@
 
 import { usePhonePePayment } from '@/app/_hooks/usePhonePePayment';
 import { Button } from '@/components/ui/button';
-import { useSession } from 'next-auth/react'; // Assuming you're using next-auth
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { toast } from 'react-hot-toast';
 
 export default function PaymentButton({ amount }: { amount: number }) {
   const { initiatePayment, loading, error } = usePhonePePayment();
-  const { data: session, status } = useSession(); // get session info
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
   const handleClick = () => {
     if (!session) {
-      toast.error('Please log in to make a payment.');
+      toast.error('Please register or log in to make a payment.');
+      router.push('/register');
       return;
     }
 
